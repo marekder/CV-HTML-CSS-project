c8079ff1b393b30ab88ba97433556a2d2ab02320
Marek Derlukiewicz <marek.derlukiewicz@gmail.com>
2022-12-01 08:37:13 +0100

    20221201 - channged background color for header

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 import './sass/main.scss'
 import AOS from 'aos'
-AOS.init()
+
+AOS.init();
+
+let options = {
+    strings: ['Front-end Developer', 'Back-end Developer'],
+    typeSpeed: 100,
+    backSpeed: 30,
+    loop: true,
+    startDelay: 500,
+};
+
+let typed = new Typed('.typed', options);
